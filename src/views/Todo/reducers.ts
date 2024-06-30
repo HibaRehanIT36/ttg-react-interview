e0e951ebe36913e066d7d646f9c06b8013c507c9
xxxxWALEEDxxxx <142427170+xxxxWALEEDxxxx@users.noreply.github.com>
2024-06-30 18:32:27 +0300

    implement added Todo actions at the reducer

--- src/views/Todo/reducers.ts
+++ src/views/Todo/reducers.ts
@@ -1,17 +1,18 @@
 import { TodoActionTypes } from './actions';
-import update from 'immutability-helper';
 import { combineReducers } from 'redux';
+import { Task } from '../../types/Task';
 
-const defaultState = {};
+const defaultState: Task[] = [];
 
 const todoReducer = (state = defaultState, action: any) => {
   switch (action.type) {
     case TodoActionTypes.addTodo:
-      return state;
+      return [...state, action.payload];
     case TodoActionTypes.deleteTodo: {
-      return state;
+      return state.filter(task => task.id !== action.payload);
     }
-
+    case TodoActionTypes.initializeTodo:
+      return action.payload; 
     default:
       return state;
   }
