17ede266f165ecfb322d037cdc76030d41318069
xxxxWALEEDxxxx <142427170+xxxxWALEEDxxxx@users.noreply.github.com>
2024-06-30 18:20:36 +0300

    define needed types for  to do  components

--- src/types/Task.ts
+++ src/types/Task.ts
@@ -1,5 +1,16 @@
 export interface Task {
-    id: number;
-    title: string;
-    description?: string;
+  id: number;
+  title: string;
+  description?: string;
+  date: string;
 }
+
+export type AddToDoProps = {
+  open: boolean;
+  handleClose: () => void;
+};
+
+export interface TaskFormValues {
+  title: string;
+  description: string;
+};
