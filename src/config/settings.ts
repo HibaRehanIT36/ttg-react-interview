ea7ab4cdc97fdcb13edfdb2c8f8428e1adfad50c
xxxxWALEEDxxxx <142427170+xxxxWALEEDxxxx@users.noreply.github.com>
2024-06-30 18:01:23 +0300

    add layouts config and routes paths to the setting file

---
+++ src/config/settings.ts
@@ -0,0 +1,21 @@
+export const settings = {
+  staleDataBeforeReFetching: 600,
+  defaultLanguage: "ar",
+};
+
+export const layoutsConfog = {
+  W_NaV_VERTICAL: 250,
+  W_NaV_Mini: 60,
+  HEADER: {
+    H_MOBILE: 10,
+    H_DESKTOP: 60,
+    H_DESKTOP_OFFSET: 90 - 16,
+    H_HEIGHT: 600,
+  },
+};
+
+export const routesPathes = {
+  dashboard: {
+    root: "/",
+    tasks: "/tasks",}
+};
